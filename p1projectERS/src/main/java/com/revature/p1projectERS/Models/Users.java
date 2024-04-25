96312e997b97bcbfe2e9ff87fa7a5a2f6a71d67d
Arun Mathew <arunmathew009@gmail.com>
2024-04-25 18:09:56 -0400

    user table added

--- p1projectERS/src/main/java/com/revature/p1projectERS/Models/Users.java
+++ p1projectERS/src/main/java/com/revature/p1projectERS/Models/Users.java
@@ -1,6 +1,21 @@
 package com.revature.p1projectERS.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Users {
+
+
+    @Id
+    private Long userId;
+
+    private String firstname;
+
+    private String lastname;
+
+
 
 
 
