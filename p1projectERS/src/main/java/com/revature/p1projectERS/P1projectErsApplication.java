6d5a7241c43d3a3c5df55460c8ef34a9bdb5559c
Arun Mathew <arunmathew009@gmail.com>
2024-04-25 12:48:25 -0400

    first commit

---
+++ p1projectERS/src/main/java/com/revature/p1projectERS/P1projectErsApplication.java
@@ -0,0 +1,13 @@
+package com.revature.p1projectERS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1projectErsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1projectErsApplication.class, args);
+	}
+
+}
---
+++ p1projectERS/src/test/java/com/revature/p1projectERS/P1projectErsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.p1projectERS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1projectErsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
